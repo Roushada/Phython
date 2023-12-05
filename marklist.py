d6c8490040c83ed4ef6df03415086379a8284dbe
Roushada <151717832+Roushada@users.noreply.github.com>
2023-12-05 15:19:38 +0530

    Add files via upload

---
+++ marklist.py
@@ -0,0 +1,11 @@
+dict={}
+x=int(input("enter the limit of student"))
+for i in range(0,5):
+           key=input("enter the student roll no");
+           value1=int(input("enter the mark of first subject"))
+           value2=int(input("enter the mark of second subject"))
+           value3=int(input("enter the mark of third subject"))
+           value4=int(input("enter the mark of fourth subject"))
+           value5=int(input("enter the mark of fiveth subject"))
+           dict[key]=value1 ,value2, value3, value4, value5
+           print (dict)
---
+++ percentage.py
@@ -0,0 +1,14 @@
+dict={}
+x=int(input("enter the limit of student"))
+for i in range(0,5):
+           key=input("enter the student roll no");
+           value1=int(input("enter the mark of first subject"))
+           value2=int(input("enter the mark of second subject"))
+           value3=int(input("enter the mark of third subject"))
+           value4=int(input("enter the mark of fourth subject"))
+           value5=int(input("enter the mark of fiveth subject"))
+           dict[key]=value1 ,value2, value3, value4, value5
+           total=value1+ value2+ value3+ value4 +value5
+           percentage=(total/500)*100
+           print("percentage of",key,"is:",percentage)
+           print (dict)
